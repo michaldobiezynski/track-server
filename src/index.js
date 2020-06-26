824fd55593b5ebd0ead96a49fd06ee3af1e2a9f0
michaldobiezynski <dobiezynski@gmail.com>
2020-06-26 09:11:44 +0100

    MongoDB Setup and connection

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+const mongoUri =
+  "mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/<dbname>?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true,
+});
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to mongo instance.");
+});
+
+mongoose.connection.on("error", (error) => {
+  console.error("Error connecting to mongo:", err);
+});
 
 app.get("/", (req, res) => {
   res.send("Hi there!");
