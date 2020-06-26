4bca65276486e039efc9126efea915437a942714
michaldobiezynski <dobiezynski@gmail.com>
2020-06-26 09:26:40 +0100

    understanding the signup process

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const authRoutes = require("./routes/authRoutes");
+
 const app = express();
+app.use(authRoutes);
 
 const mongoUri =
   "mongodb+srv://michal123:michal123@devconnector-6tf9d.mongodb.net/<dbname>?retryWrites=true&w=majority";
