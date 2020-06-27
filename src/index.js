949c336c2dbf6b3bf8ed1a0f41dec4b7a99ec69b
michaldobiezynski <dobiezynski@gmail.com>
2020-06-27 16:37:22 +0100

    wiring up JSON Web Token

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
+const requireAuth = require("./middlewares/requireAuth");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
@@ -27,8 +28,8 @@ mongoose.connection.on("error", (error) => {
   console.error("Error connecting to mongo:", err);
 });
 
-app.get("/", (req, res) => {
-  res.send("Hi there!");
+app.get("/", requireAuth, (req, res) => {
+  res.send(`Your email: ${req.user.email}`);
 });
 
 app.listen(3000, () => {
